ca47e938984fafc64dba6ca42a7ba373656bd18c
Rhett Fereday <gmfereday@gmail.com>
2020-02-06 18:05:07 -0600

    This is the initial development commit.
    This commit contains initial basic testing of the game premise in a non-AR environment. Items included/currently being worked on are marble collision details (trying to emulate real interactions).
    Basic physics materials for the marbles, the main board, and the felt region have been created - friction coefficients still need adjustment. Marble mass, particularly for the scoring marble,
    is not quite correct.

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public float force = 3.0f;
+    public Vector3 direction = new Vector3(0.0f, 0.0f, 1.0f);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Rigidbody body = this.GetComponent<Rigidbody>();
+            body.AddForce(direction * force, ForceMode.Impulse);
+        }
+    }
+}
