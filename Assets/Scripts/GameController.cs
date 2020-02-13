ee3132d92e59606d327f64f9c20e85eee74a782a
vrp56 <38020844+vrp56@users.noreply.github.com>
2020-02-12 23:53:13 -0600

    Begining of AR Integration
    The large baord can be placed in AR

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+public class GameController : MonoBehaviour
+{
+    public Camera FirstPersonCamera;
+
+    private Anchor anchor;
+    private DetectedPlane detectedPlane;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void SetSelectedPlane(DetectedPlane detectedPlane)
+    {
+        this.detectedPlane = detectedPlane;
+        CreateAnchor();
+    }
+
+    void CreateAnchor()
+    {
+        // create an anchor position by raycasting a point to the center of the screen
+        Vector2 position = new Vector2(Screen.width * .5f, Screen.height * .5f);
+        Ray ray = FirstPersonCamera.ScreenPointToRay(position);
+        Vector3 anchorPosition = detectedPlane.CenterPose.position;
+
+        // create an anchor at that point
+        if (anchor != null)
+        {
+#pragma warning disable CS0618 // Type or member is obsolete
+            DestroyObject(anchor);
+#pragma warning restore CS0618 // Type or member is obsolete
+        }
+        anchor = detectedPlane.CreateAnchor(new Pose(anchorPosition, Quaternion.identity));
+
+        // attach the object to the anchor
+        transform.position = anchorPosition;
+        transform.SetParent(anchor.transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // tracking state must be FrameTrackingState.Tracking to access Frame
+        if (Session.Status != SessionStatus.Tracking)
+        {
+            return;
+        }
+
+        // if there is no plane, return
+        if (detectedPlane == null)
+        {
+            return;
+        }
+
+        
+    }
+}
--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,18 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using GoogleARCore;
 
 public class SceneController : MonoBehaviour
 {
+    public Camera firstPersonCamera;
+    public GameController gameObj;
+    
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    void SetSelectedPlane(DetectedPlane selectedPlane)
+    {
+        Debug.Log("Selected plane centered at: " + selectedPlane.CenterPose.position);
+
+        gameObj.SetSelectedPlane(selectedPlane);
+    }
+
+    void ProcessTouches()
+    {
+        Touch touch;
+        if (Input.touchCount != 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+        {
+            return;
+        }
+
+        TrackableHit hit;
+        TrackableHitFlags raycastFilter =
+            TrackableHitFlags.PlaneWithinBounds |
+            TrackableHitFlags.PlaneWithinPolygon;
+
+        if (Frame.Raycast(touch.position.x, touch.position.y, raycastFilter, out hit))
+        {
+            SetSelectedPlane(hit.Trackable as DetectedPlane);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        // Session has to be tracking in order to access Frame
+        if (Session.Status != SessionStatus.Tracking)
+        {
+            int lostTrackingSleepTimeout = 15;
+            Screen.sleepTimeout = lostTrackingSleepTimeout;
+            return;
+        }
+
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+
+        ProcessTouches();
     }
 }
