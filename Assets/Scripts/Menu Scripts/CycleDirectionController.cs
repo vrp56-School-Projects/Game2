58f2c23db71d2e968c3f56811d7ab8eb3ebd00e8
Rhett Fereday <gmfereday@gmail.com>
2020-02-12 22:58:28 -0600

    Updated arrow textures and added some functionality to menus. Option cyclers now successfully cycle through the names of the different options they are set to represent. Also the arrows for cyclers now look very nice and a matching confirmation button was created.

---
+++ Assets/Scripts/Menu Scripts/CycleDirectionController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CycleDirectionController : MonoBehaviour
+{
+    [SerializeField]
+    private int _direction = 1;
+
+    private CycleOptionController _cycleOptionScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cycleOptionScript = this.GetComponentInParent<CycleOptionController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        //The Direction arrow has been clicked. Cycle the selected option in the configured direction
+
+        if (_direction == 1) _cycleOptionScript.CycleRight();
+
+        else _cycleOptionScript.CycleLeft();
+    }
+}
---
+++ Assets/Scripts/Menu Scripts/CycleOptionController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CycleOptionController : MonoBehaviour
+{
+    private int _currentIndex = 0;
+
+    //[SerializeField]
+    //private GameObject[] _options;
+
+    [SerializeField]
+    private string[] _optionNames;
+
+    private TMPro.TextMeshPro _textMesh;
+
+    public GameObject test;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //if (_optionNames.Length != _options.Length) Debug.Log("Error: The number of options and number of option names does not match!");
+
+        foreach(string name in _optionNames)
+        {
+            Debug.Log(name.ToString());
+        }
+
+        _textMesh = test.GetComponent<TMPro.TextMeshPro>();
+        _textMesh.text = _optionNames[_currentIndex];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CycleRight()
+    {
+        //Cycle right and handle wrap-around
+        _currentIndex = (_currentIndex + 1) % _optionNames.Length;
+        _textMesh.text = _optionNames[_currentIndex];
+    }
+
+    public void CycleLeft()
+    {
+        //Cycle left and handle wrap-around
+        _currentIndex = (_currentIndex + _optionNames.Length - 1) % _optionNames.Length;
+        _textMesh.text = _optionNames[_currentIndex];
+    }
+}
