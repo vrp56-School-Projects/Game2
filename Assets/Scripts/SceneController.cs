9561dfad38122c03e3005149c6c765d25135bc21
Rhett Fereday <gmfereday@gmail.com>
2020-02-17 15:46:18 -0600

    Basic preparations for game state management have been implemented.

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -14,18 +14,45 @@ public class SceneController : MonoBehaviour
     private GameObject[] _marbleHats;
 
     [SerializeField]
-    private GameObject _marblePrefab;
+    private GameObject _playerMarblePrefab;
+
+    [SerializeField]
+    private GameObject _scoreMarblePrefab;
 
     [SerializeField]
     private Vector3 _marbleStartPosition;
 
+    //Player 1 Information
     private List<GameObject> _player1Marbles;
+    private int _player1TextureIndex;
+    private int _player1TrailIndex;
+    private int _player1HatIndex;
+
+    //Player 2 Information
     private List<GameObject> _player2Marbles;
+    private int _player2TextureIndex;
+    private int _player2TrailIndex;
+    private int _player2HatIndex;
 
     private GameObject _demoMarble;
 
     [SerializeField]
     private GameObject _playerOptionsMenu;
+
+    //The Possible Game States
+    private enum GameState
+    {
+        BOARD_PLACEMENT,
+        BOARD_SELECTION,
+        PLAYER1_OPTIONS,
+        PLAYER2_OPTIONS,
+        PLAYER1_TURN,
+        PLAYER2_TURN,
+        END_GAME
+    }
+
+    //Track the current game state
+    private GameState _currentState = GameState.PLAYER1_OPTIONS;
 
     // Start is called before the first frame update
     void Start()
@@ -33,9 +60,9 @@ public class SceneController : MonoBehaviour
         _player1Marbles = new List<GameObject>();
         _player2Marbles = new List<GameObject>();
 
-        _demoMarble = Instantiate(_marblePrefab, new Vector3(_playerOptionsMenu.transform.position.x, -0.55f, _playerOptionsMenu.transform.position.z), Quaternion.identity, _playerOptionsMenu.transform);
+        //Create and show the menu with the orbiting marble
+        _demoMarble = Instantiate(_playerMarblePrefab, new Vector3(_playerOptionsMenu.transform.position.x, -0.55f, _playerOptionsMenu.transform.position.z), Quaternion.identity, _playerOptionsMenu.transform);
         _demoMarble.AddComponent<MarbleOrbitController>();
-
         _playerOptionsMenu.GetComponent<MenuVisibilityController>().Show();
 
         SetMarbleTexture(0);
@@ -47,10 +74,6 @@ public class SceneController : MonoBehaviour
     void Update()
     {
         
-    }
-
-    private void SpawnPlayerMarble()
-    {
     }
 
     public void SetMarbleTexture(int index)
@@ -70,6 +93,34 @@ public class SceneController : MonoBehaviour
 
     public void MenuClosed()
     {
+        switch(_currentState)
+        {
+            case GameState.BOARD_SELECTION:
+                _currentState = GameState.PLAYER1_OPTIONS;
+                break;
+
+            case GameState.PLAYER1_OPTIONS:
+                break;
+
+            case GameState.PLAYER2_OPTIONS:
+                break;
+        }
         _playerOptionsMenu.GetComponent<MenuVisibilityController>().Hide();
+    }
+
+    public void EndTurn()
+    {
+        switch(_currentState)
+        {
+            case GameState.PLAYER1_TURN:
+                _currentState = GameState.PLAYER2_TURN;
+                break;
+
+            case GameState.PLAYER2_TURN:
+                _currentState = GameState.PLAYER1_TURN;
+                break;
+        }
+
+        if (_player2Marbles.Count >= 5) _currentState = GameState.END_GAME;
     }
 }
